914a31b819f7f43f62d31bace5664d177a697ab7
L <lorenzomrdev@gmail.com>
2025-10-20 23:59:46 -0400

    dumb

---
+++ LeetCode/LC3330.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int total = 0;
+        for(int i = 1; i < word.length(); i++){
+            if(word[i-1] == word[i]){
+                total++;
+            }
+        }
+        return total +1;
+    }
+};
